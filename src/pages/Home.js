501378e0bf9a1da661c1ed447ac607588fc4ae28
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-29 12:01:06 -0500

    functionality to get more products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,22 +6,33 @@ import '../styles/home.css';
 
 const Home = () => {
   const [listProducts, setListProducts] = useState([]);
+  const [limit, setLimit] = useState(7);
 
   useEffect(() => {
-    axios.get('https://api.escuelajs.co/api/v1/products?limit=7&offset=0').then(response => {
+    axios.get(`https://api.escuelajs.co/api/v1/products?limit=${limit}&offset=0`).then(response => {
       console.log(response);
       setListProducts(response.data);
     });
-  }, []);
+  }, [limit]);
 
   return (
-    <section className="container-cards">
-      { listProducts &&
-        listProducts.map(product =>
-          <ProductItem key={ product.id } producData={ product } />
-        )
-      }
-    </section>
+    <>
+      <section className="container-cards">
+        { listProducts &&
+          listProducts.map(product =>
+            <ProductItem key={ product.id } producData={ product } />
+          )
+        }
+      </section>
+      <div className="d-flex justify-content-center mb-2">
+        <button type="button" className="load-more-button"
+          onClick={ () => setLimit(limit + 5) }
+        >
+          Cargar 5 Productos Mas
+          <i className="bi bi-arrow-down-circle-fill" style={ {fontSize: '1.5rem', marginLeft: '0.5rem'} }></i>
+        </button>
+      </div>
+    </>
   );
 };
 
