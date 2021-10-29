4064917fe00ae5e29b8d5d3eb285ba49d5ce1069
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-29 13:44:21 -0500

    added SEO

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Switch} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import {Suspense, lazy} from 'react';
+import Helmet from 'react-helmet';
 
 import { AuthProvider } from './contexts/Auth';
 import { ShoppingCartProvider } from './contexts/ShoppingCart';
@@ -32,21 +33,37 @@ const App = () => {
             <Switch>
               <PublicRoute exact path="/">
                 <Suspense fallback={ <SpinnerLoading /> }>
+                  <Helmet>
+                    <title>Inicio | YardSale</title>
+                    <meta name="description" content="Sitio web venta de garaje manipulando rutas, aplicando operaciones con variables estáticas (volatiles)." />
+                  </Helmet>
                   <Index />
                 </Suspense>
               </PublicRoute>
               <PublicRoute exact path="/login">
                 <Suspense fallback={ <SpinnerLoading /> }>
+                  <Helmet>
+                    <title>Iniciar Sesión | YardSale</title>
+                    <meta name="description" content="Inicia sesión para poder ver el tablero para consumir una API que devuelve datos aleatorios simulando productos." />
+                  </Helmet>
                   <Login />
                 </Suspense>
               </PublicRoute>
               <PublicRoute exact path="/register">
                 <Suspense fallback={ <SpinnerLoading /> }>
+                  <Helmet>
+                    <title>Registrarse | YardSale</title>
+                    <meta name="description" content="Regístrate para comprobar el funcionamiento del formulario, estos datos solo se validan mas no se guardan." />
+                  </Helmet>
                   <Register />
                 </Suspense>
               </PublicRoute>
               <PrivateRouter exact path="/home">
                 <Suspense fallback={ <SpinnerLoading /> }>
+                  <Helmet>
+                    <title>Yard Sale</title>
+                    <meta name="description" content="Sitio web venta de garaje mostrando productos aleatorios filtrando también por categorías y poder agregar productos al carrito." />
+                  </Helmet>
                   <Home />
                 </Suspense>
               </PrivateRouter>
