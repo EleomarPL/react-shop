3aadb1b9dd3c7a918d61de4c6d39b9cf2ec6eaf9
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-10-29 08:27:44 -0500

    added routers and install dependences

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import {BrowserRouter, Switch} from 'react-router-dom';
+
+
 import { AuthProvider } from './contexts/Auth';
+import { ShoppingCartProvider } from './contexts/ShoppingCart';
+
 
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,13 +12,29 @@ import '@fontsource/roboto';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './styles/index.css';
-import { ShoppingCartProvider } from './contexts/ShoppingCart';
+import PublicRoute from './components/router/PublicRouter';
+import PrivateRouter from './components/router/PrivateRouter';
 
 const App = () => {
   return (
     <AuthProvider>
       <ShoppingCartProvider>
-        
+        <BrowserRouter>
+          <Switch>
+            <PublicRoute exact path="/">
+              <p>Introduction</p>
+            </PublicRoute>
+            <PublicRoute exact path="/login">
+              <p>Login</p>
+            </PublicRoute>
+            <PublicRoute exact path="/register">
+              <p>Register</p>
+            </PublicRoute>
+            <PrivateRouter exact path="/home">
+              <p>Home</p>
+            </PrivateRouter>
+          </Switch>
+        </BrowserRouter>
       </ShoppingCartProvider>
     </AuthProvider>
   );
